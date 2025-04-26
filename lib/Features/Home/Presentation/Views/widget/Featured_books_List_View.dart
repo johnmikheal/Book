a1d1e94cf5e0ec3d9844f6be0d9846655d720b3a
johnmikheal <johnmikheal@gmail.com>
2025-04-26 03:16:00 -0800

    create style file & title

---
+++ lib/Features/Home/Presentation/Views/widget/Featured_books_List_View.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:tkuo/Features/Home/Presentation/Views/widget/Custom_List_View_Item.dart';
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: CustomListViewItem(),
+            );
+          }),
+    );
+  }
+}
--- lib/Features/Home/Presentation/Views/widget/Home_View_Body.dart
+++ lib/Features/Home/Presentation/Views/widget/Home_View_Body.dart
@@ -1,37 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tkuo/Core/Utilites/Styles.dart';
 import 'package:tkuo/Features/Home/Presentation/Views/widget/Custom_App_Bar.dart';
 import 'package:tkuo/Features/Home/Presentation/Views/widget/Custom_List_View_Item.dart';
+import 'package:tkuo/Features/Home/Presentation/Views/widget/Featured_books_List_View.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItem(),
-            );
-          }),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
