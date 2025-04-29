e8831773a552586bd7e31603d8a3b509e4f8b397
johnmikheal <johnmikheal@gmail.com>
2025-04-29 08:59:08 -0800

    best seller item part2

--- lib/Constans.dart
+++ lib/Constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 const KprimaryColor = Color(0xff100B20);
+const KGtSectraFine = 'GT Sectra Fine';
--- lib/Features/Home/Presentation/Views/Home_View.dart
+++ lib/Features/Home/Presentation/Views/Home_View.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tkuo/Features/Home/Presentation/Views/widget/Home_View_Body.dart';
 
 class HomeView extends StatelessWidget {
