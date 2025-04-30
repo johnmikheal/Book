fc491fe34f327622a6139c3e8f2152fd5a76228c
johnmikheal <johnmikheal@gmail.com>
2025-04-30 01:37:36 -0800

    best seller item done

--- lib/Features/Home/Presentation/Views/widget/Best_Seller_List_View_Item.dart
+++ lib/Features/Home/Presentation/Views/widget/Best_Seller_List_View_Item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tkuo/Core/Utilites/Assets.dart';
 import 'package:tkuo/Core/Utilites/Styles.dart';
+import 'package:tkuo/Features/Home/Presentation/Views/widget/Book_Rating.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -9,7 +11,7 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 125,
+      height: 115,
       child: Row(
         children: [
           AspectRatio(
@@ -25,48 +27,51 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and the Goblent of Fire ',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: const Text(
+                    'Harry Potter and the Goblent of Fire ',
+                    style: Styles.textStyle20,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                ' j.k rolyent',
-                style: Styles.textStyle14,
-              ),
-              SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  Text(
-                    '19.99 ',
-                    style: Styles.textStyle20
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  Icon(Icons.euro),
-                  Icon(
-                    Icons.star,
-                    color: Colors.amberAccent,
-                  ),
-                  Text('4.8'),
-                  Text('(2390)')
-                ],
-              )
-            ],
+                const SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  ' j.k rolyent',
+                  style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 ',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Icon(Icons.euro),
+                    const Spacer(
+                      flex: 3,
+                    ),
+                    const BookRating(),
+                    const Spacer(
+                      flex: 2,
+                    ),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
--- lib/Features/Home/Presentation/Views/widget/Home_View_Body.dart
+++ lib/Features/Home/Presentation/Views/widget/Home_View_Body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tkuo/Core/Utilites/Assets.dart';
 import 'package:tkuo/Core/Utilites/Styles.dart';
 import 'package:tkuo/Features/Home/Presentation/Views/widget/Best_Seller_List_View_Item.dart';
